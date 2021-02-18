ff3fa1352356a77fc3cbd9c6dc12157cde776892
saltKosher <saltswapfinance@gmail.com>
2021-02-19 07:40:59 +1100

    feat: Add staked only toggle

--- src/views/Farms/components/FarmTabButtons/index.tsx
+++ src/views/Farms/components/FarmTabButtons/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useRouteMatch, Link } from 'react-router-dom'
-import { ButtonMenu, ButtonMenuItem } from '@saltswap/uikit'
+import { ButtonMenu, ButtonMenuItem, Toggle, Text } from '@saltswap/uikit'
 import useI18n from 'hooks/useI18n'
 
-const FarmTabButtons = () => {
+const FarmTabButtons = ({ stackedOnly, setStackedOnly }) => {
   const { url, isExact } = useRouteMatch()
   const TranslateString = useI18n()
 
   return (
     <Wrapper>
+      <ToggleWrapper>
+        <Toggle checked={stackedOnly} onChange={() => setStackedOnly(!stackedOnly)} />
+        <Text> {TranslateString(1116, 'Staked only')}</Text>
+      </ToggleWrapper>
       <ButtonMenu activeIndex={!isExact ? 1 : 0} size="sm" variant="subtle">
         <ButtonMenuItem as={Link} to={`${url}`}>
           {TranslateString(999, 'Active')}
@@ -29,4 +33,15 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   margin-bottom: 32px;
+`
+
+const ToggleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 32px;
+
+  ${Text} {
+    margin-left: 8px;
+  }
 `
