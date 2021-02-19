e67429c4652d0f0e847b07d2d2e402202a26679c
saltPink <filipe.nasto86@gmail.com>
2021-02-20 00:26:07 +0200

    feat: Adds some pools and contracs

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,11 +1,11 @@
 export default {
   cake: {
     56: '0x89dcddca577f3658a451775d58ea99da532263c8', // FIXME: SALT TOKEN
-    97: '0x89dcddca577f3658a451775d58ea99da532263c8', // FIXME: SALT TOKEN
+    97: '0xDE7FD400F6091c5786271A3aA6E7587Ae60390D6',
   },
   masterChef: {
     56: '0xd37441c3adc637e43f96881d8b3898f433269d11', // FIXME: SALT MASTERCHEF
-    97: '0xd37441c3adc637e43f96881d8b3898f433269d11', // FIXME: SALT MASTERCHEF
+    97: '0xa5f8061Daf38E813dB352069560FCAeC061594f9',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
@@ -26,5 +26,9 @@ export default {
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
     97: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
+  },
+  eth: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // FIXME:
+    97: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee', // FIXME:
   },
 }
