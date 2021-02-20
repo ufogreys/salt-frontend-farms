cc13b33789a4c14eea141914820be79efab0513d
saltKosher <saltswapfinance@gmail.com>
2021-02-21 09:03:11 +1100

    feat: Add Salt Bae to the homepage

--- src/views/Home/components/FarmStakingCard.tsx
+++ src/views/Home/components/FarmStakingCard.tsx
@@ -10,7 +10,7 @@ import CakeHarvestBalance from './CakeHarvestBalance'
 import CakeWalletBalance from './CakeWalletBalance'
 
 const StyledFarmStakingCard = styled(Card)`
-  background-image: url('/images/salt-bg.svg');
+  background-image: url('/images/salt-bae-bg.png');
   background-repeat: no-repeat;
   background-position: top right;
   min-height: 376px;
