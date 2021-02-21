7701df4a953506677ceb8f36e93d3b7e0e5a2f62
saltPink <filipe.nasto86@gmail.com>
2021-02-21 03:22:44 +0200

    feat: Updates dev to prod, updates LP for SALT

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -20,7 +20,7 @@ export default {
     97: '',
   },
   mulltiCall: {
-    56: '0x2b4b36861769060c9B3ff8199d2343F731db240D',
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
     97: '0x2b4b36861769060c9B3ff8199d2343F731db240D',
   },
   busd: {
--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -8,8 +8,8 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'SALT',
     lpAddresses: {
-      97: '0x0000000000000000000000000000000000000000',
-      56: '0x0000000000000000000000000000000000000000',
+      97: '0xf62e8d3EcA464C9Fa9D46f169211eF4a41E41fE8',
+      56: '0x6596f770786915556C47E301cC8290aa14288d99', // SALT-BUSD
     },
     tokenSymbol: 'SALT',
     tokenAddresses: {
@@ -50,7 +50,7 @@ const farms: FarmConfig[] = [
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb,
-  }, /*
+  } /*
   {
     pid: 3,
     risk: 1,
@@ -164,7 +164,7 @@ const farms: FarmConfig[] = [
     },
     quoteTokenSymbol: QuoteToken.ETH,
     quoteTokenAdresses: contracts.eth,
-  }, */
+  }, */,
 ]
 
 export default farms
