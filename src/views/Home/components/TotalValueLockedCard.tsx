2d23c7a59264372eace3b3672f1f9afa3f0711d7
saltPink <filipe.nasto86@gmail.com>
2021-03-12 12:50:16 +0200

    feat: Add Oceans name to TVL

--- src/views/Home/components/TotalValueLockedCard.tsx
+++ src/views/Home/components/TotalValueLockedCard.tsx
@@ -28,7 +28,7 @@ const TotalValueLockedCard = () => {
           {/* <Heading size="xl"> */}
           <CardValue value={totalValue.toNumber()} prefix="$" decimals={2} />
           {/* </Heading> */}
-          <Text color="textSubtle">{TranslateString(999, 'Across all Farms and Pools')}</Text>
+          <Text color="textSubtle">{TranslateString(999, 'Across all Farms, Pools and Oceans')}</Text>
         </>
       </CardBody>
     </StyledTotalValueLockedCard>
