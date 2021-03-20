07c568d82f224aa920f3eeb2cbc9cf727c296658
saltPink <filipe.nasto86@gmail.com>
2021-03-20 13:39:13 +0200

    feat: Add RebaseProtocol

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -2,9 +2,31 @@ import { Ifo } from './types'
 
 const ifos: Ifo[] = [
   {
-    id: 'momoprotocol',
+    id: 'rebasingprotocol',
     address: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
     isActive: true,
+    name: 'RebasingProtocol',
+    token: 'RBT',
+    subTitle: 'The 1st BSC project that offers Flashloan Arbitrage & Rewards Contributors through our Social App',
+    description:
+      'MomoProtocol is an online platform for using, sharing, and getting information related to any field of knowledge. It is a website that will contain the questions and answers and all those questions and answers will be rewarded through tokens of the website. MomoProtocol is also going to Launch Flashloan Arbitrage on BSC. Where users will need Momo Tokens to try the Flashloan without any collateral.',
+    launchDate: 'Mar. 20',
+    launchTime: '4PM CET',
+    saleAmount: '5000',
+    raiseAmount: '400',
+    cakeToBurn: '0',
+    projectSiteUrl: 'https://rebasing.live',
+    currency: 'BNB',
+    currencyAddress: '0xDd3F196c59Df7bdABc18BBcfFb6440298AE117e2', // FIX This is WBNB
+    tokenDecimals: 18,
+    releaseBlockNumber: 7086064,
+    maxContribution: '50',
+    minContribution: '0.1',
+  },
+  {
+    id: 'momoprotocol',
+    address: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+    isActive: false,
     name: 'MomoProtocol',
     token: 'MOMO',
     subTitle: 'The 1st BSC project that offers Flashloan Arbitrage & Rewards Contributors through our Social App',
